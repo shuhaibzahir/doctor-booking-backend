ea7dc4d2deb84d23ebb306994874a568a1dd5b99
shuhaibzahir <yesshuhaib.ss@gmail.com>
2022-04-30 14:17:12 +0530

    functions implimented

--- src/controllers/Hospital/auth.js
+++ src/controllers/Hospital/auth.js
@@ -1,4 +1,6 @@
 const hospitalauth = require('../../services/Hospital/auth')
+
+/* ---------------------- this is for hospital register --------------------- */
 const register= (req,res,next)=>{
     console.log(req.body)
     hospitalauth.hospitalRegister(req.body).then((data)=>{
